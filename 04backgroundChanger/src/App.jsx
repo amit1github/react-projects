b649d47adcfe2cf9ddb6fce5d4354f8031e6c87e
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2023-11-21 18:52:18 +0530

    feat: background changer

--- 04backgroundChanger/src/App.jsx
+++ 04backgroundChanger/src/App.jsx
@@ -1,18 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("pink");
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button
+              onClick={() => {
+                setColor("red");
+              }}
+              style={{ backgroundColor: "red" }}
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            >
+              Red
+            </button>
+            <button
+              onClick={() => setColor("green")}
+              style={{ backgroundColor: "green" }}
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            >
+              Green
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
